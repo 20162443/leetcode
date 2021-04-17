8a4aadac1502d4ccb925a42f0fd8398772e1ac08
root <390981874@qq.com>
2021-04-17 22:36:23 +0800

    树的后序遍历

---
+++ leetcode/src/main/java/Tree/PostOrder.java
@@ -0,0 +1,50 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class PostOrder {
+
+    public List<Integer> postorderTraversal(TreeNode2 root) {
+        List<Integer> list=new ArrayList<Integer>();
+        Stack<TreeNode2> stack=new Stack<TreeNode2>();
+        TreeNode2 p=root;
+        TreeNode2 t=null;
+        int i=0;
+        while(!stack.isEmpty()||p!=null){
+            i++;
+            if(p!=null){
+                stack.push(p);
+                p=p.left;
+            }else{
+                p=stack.peek();
+
+                if(p.right!=null&&t!=p.right){
+                    p=p.right;
+                    stack.push(p);
+                    p=p.left;
+                }else{
+                    p=stack.pop();
+                    list.add(p.val);
+                    t=p;
+                    p=null;
+                }
+            }
+        }
+        return list;
+
+    }
+}
+class TreeNode2 {
+    int val;
+    TreeNode2 left;
+    TreeNode2 right;
+    TreeNode2() {}
+    TreeNode2(int val) { this.val = val; }
+    TreeNode2(int val, TreeNode2 left, TreeNode2 right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
